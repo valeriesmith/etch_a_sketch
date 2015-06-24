4728193e3eeda273a907bc5b592d26a416f53064
Valerie Smith <valeriesmithdbc@gmail.com>
2015-06-24 14:52:56 -0700

    builds grid with user input error checking to make sure text only contains digits

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 
 function populateGrid(gridSize) {
+	console.log("inside populate")
+	console.log(gridSize)
 	for (i = 1; i <= gridSize; i ++) {
 		$("#gridtable").append("<tr id=row" + i + "></tr>");
 		for (j = 1; j <= gridSize; j ++) {
@@ -8,9 +10,23 @@ function populateGrid(gridSize) {
 	}
 }
 
-var gridSize = 4
+$("form").on("submit", function(event) {
+	var sizeInput = $("input:text").val();
+	var gridSizeRegex = /^\d*$/;
+	if (!gridSizeRegex.test(sizeInput)) {
+		alert("you must put in a number");
+		$("#gridsize").val("");
+		event.preventDefault();
+	} else {
+		gridSize = sizeInput;
+		event.preventDefault();
+		populateGrid(gridSize)
+	}	
+});
 
-$( document ).ready(populateGrid(4));
+// console.log(gridSize)
+
+// $( document ).ready(populateGrid(4));
 
 
 
